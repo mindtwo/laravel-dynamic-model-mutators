941100f7b8a3a54d4f1ff97d9bafbb20b1bebe0a
Tobi Kivelip <kivelip@mindtwo.de>
2018-11-29 16:18:49 +0100

    Added fillable tests

--- tests/DynamicModelMutatorTest.php
+++ tests/DynamicModelMutatorTest.php
@@ -5,9 +5,46 @@ namespace Tests;
 use Illuminate\Database\Eloquent\Model;
 use mindtwo\LaravelDynamicModelMutators\DynamicModelMutator;
 use mindtwo\LaravelDynamicModelMutators\Exceptions\MutatorNotCallableException;
+use Mockery\Mock;
 
 class DynamicModelMutatorTest extends TestCase
 {
+    /**
+     * Mock eloquent model with dynamic model mutator trait.
+     *
+     * @return Model
+     */
+    protected function eloquentModelWithTrait()
+    {
+        return new class() extends Model {
+            use DynamicModelMutator;
+
+            protected $test = [
+                'custom_property',
+                'custom_fillable_property',
+            ];
+
+            protected $fillable = [
+                'custom_fillable_property',
+            ];
+
+            protected $testValues = [
+                'custom_property'          => 'TEST',
+                'custom_fillable_property' => 'TEST',
+            ];
+
+            public function getAttributeTest(string $attribute): string
+            {
+                return $this->testValues[$attribute];
+            }
+
+            public function setAttributeTest(string $key, $value)
+            {
+                $this->testValues[$key] = $value;
+            }
+        };
+    }
+
     public function testRegisterGetMutator()
     {
         $model = $this->eloquentModelWithTrait();
@@ -28,6 +65,32 @@ class DynamicModelMutatorTest extends TestCase
         $this->assertEquals('Example', $model->custom_property);
     }
 
+    public function testFillMutatedAttributes()
+    {
+        $model = $this->eloquentModelWithTrait();
+
+        $model::registerSetMutator('test', 'setAttributeTest');
+
+        $model->fill([
+            'custom_fillable_property' => 'Example',
+        ]);
+
+        $this->assertEquals('Example', $model->getAttributeTest('custom_fillable_property'));
+    }
+
+    public function testAttributeIsNotFilledIfUndefined()
+    {
+        $model = $this->eloquentModelWithTrait();
+
+        $model::registerSetMutator('test', 'setAttributeTest');
+
+        $model->fill([
+            'custom_property' => 'Example',
+        ]);
+
+        $this->assertEquals('TEST', $model->getAttributeTest('custom_property'));
+    }
+
     public function testThrowExceptionWhenSetterMethodNameIsNotCallable()
     {
         $model = $this->eloquentModelWithTrait();
@@ -44,25 +107,5 @@ class DynamicModelMutatorTest extends TestCase
         $this->expectException(MutatorNotCallableException::class);
 
         $model::registerGetMutator('test', 'noneExistingMethod');
-    }
-
-    protected function eloquentModelWithTrait()
-    {
-        return new class() extends Model {
-            use DynamicModelMutator;
-
-            protected $test = ['custom_property'];
-            protected $testValues = ['custom_property' => 'TEST'];
-
-            public function getAttributeTest(string $attribute): string
-            {
-                return $this->testValues[$attribute];
-            }
-
-            public function setAttributeTest(string $key, $value)
-            {
-                $this->testValues[$key] = $value;
-            }
-        };
     }
 }
